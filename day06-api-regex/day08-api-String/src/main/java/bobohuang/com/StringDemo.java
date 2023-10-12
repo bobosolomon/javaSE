c40f9e22c8c61c921a9e9d3c6c3f48ef5b4395a5
bobosolomon <bobosolomon@163.com>
2023-10-12 18:54:26 +0800

    学习java的api String

---
+++ day06-api-regex/day08-api-String/src/main/java/bobohuang/com/StringDemo.java
@@ -0,0 +1,24 @@
+package bobohuang.com;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        // 1、直接双引号得到字符串对象，封装字符串数据
+        String name = "黑马666";
+        System.out.println(name);
+
+        // 2、new String创建字符串对象，并调用构造器初始化字符串
+        String rs1 = new String();
+        System.out.println(rs1); // ""
+
+        String rs2 = new String("itheima");
+        System.out.println(rs2);
+
+        char[] chars = {'a', '黑', '马'};
+        String rs3 = new String(chars);
+        System.out.println(rs3);
+
+        byte[] bytes = {97, 98, 99};
+        String rs4 = new String(bytes);
+        System.out.println(rs4);
+    }
+}
